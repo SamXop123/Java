a429293bbfbfb60fc3f146584afdd8b1c488b907
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-10-09 20:58:53 +0530

    Create SystemCalls.java

---
+++ SystemCalls.java
@@ -0,0 +1,15 @@
+public class SystemInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Operating System: " + System.getProperty("os.name"));
+
+        System.out.println("Version: " + System.getProperty("os.version"));
+
+        System.out.println("Architecture: " + System.getProperty("os.arch"));
+
+        System.out.println("User: " + System.getProperty("user.name"));
+
+        System.out.println("Home Directory: " + System.getProperty("user.home"));
+
+    }
+}
