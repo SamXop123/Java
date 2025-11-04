2b6473e1f6c5d0f9a20aea60d1878aa767d875b1
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-11-04 20:05:02 +0530

    Create YourHeart.java

---
+++ DSA/Patterns/YourHeart.java
@@ -0,0 +1,52 @@
+package DSA.Patterns;
+
+// Heart Pattern
+
+/*
+notes:
+this heart pattern is made by combining two circle-like parts on top and a triangle shape at the bottom. 
+the upper part uses the circle equation (x² + y² = r²) logic with sqrt() to decide where to print stars. 
+the lower part just reduces stars line by line to make that pointed bottom shape. looks fancy but it's mostly math + nested loops magic :)
+*/
+
+public class YourHeart {
+    public static void main(String[] args) {
+      
+        final int size = 4  ;  
+
+        // upper part of Heart  
+        for (int m = 0; m < size; m++) {  
+
+            for (int k = 0; k <= 4 * size; k++) {  
+
+                double pos1 = Math.sqrt(Math.pow(m - size, 2) + Math.pow(k - size, 2));  
+                double pos2 = Math.sqrt(Math.pow(m - size, 2) + Math.pow(k - 3 * size, 2));  
+
+                if (pos1 < size + 0.5 || pos2 < size + 0.5) {  
+                        System.out.print('*');  
+                } 
+                else {  
+                        System.out.print(' ');  
+                }  
+            }  
+            System.out.print(System.lineSeparator());  
+        }  
+
+            // lower part of Heart  
+            for (int m = 1; m <= 2 * size; m++)  {  
+
+                for (int k = 0; k < m; k++) {  
+
+                    System.out.print(' ');  
+            }  
+
+            for (int k = 0; k < 4 * size + 1 - 2 * m; k++) {  
+
+                System.out.print("*");  
+            }  
+            System.out.print(System.lineSeparator());  
+            }  
+            System.out.println();
+
+   }
+}   
