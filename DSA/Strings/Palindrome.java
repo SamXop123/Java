65d4c37bfc9035368a7aca3ef441dd174d3244f1
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-11-21 00:43:32 +0530

    Create Palindrome.java

---
+++ DSA/Strings/Palindrome.java
@@ -0,0 +1,20 @@
+package DSA.Strings;
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+      
+        System.out.print("Enter a word: ");
+        String str = sc.nextLine();
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(reversed)) {
+            System.out.println("It's a palindrome!");
+        } else {
+            System.out.println("Not a palindrome.");
+        }
+        sc.close();
+    }
+}
