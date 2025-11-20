09882fa12d87b34b512f49dbe6e36c394a09c0b0
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-11-21 00:43:35 +0530

    Create BST.java

---
+++ DSA/Trees/BST.java
@@ -0,0 +1,65 @@
+package DSA.Trees;
+
+class BST {
+  
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int value) {
+            data = value;
+            left = right = null;
+        }
+    }
+
+    Node root;
+
+    BST() {
+        root = null;
+    }
+
+    // Insert into BST
+    Node insert(Node root, int value) {
+        if (root == null) {
+            root = new Node(value);
+            return root;
+        }
+        if (value < root.data)
+            root.left = insert(root.left, value);
+        else if (value > root.data)
+            root.right = insert(root.right, value);
+        return root;
+    }
+
+    void insert(int value) {
+        root = insert(root, value);
+    }
+
+    // Inorder Traversal
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    void inorder() {
+        inorder(root);
+    }
+
+
+  public static void main(String[] args) {
+        BST tree = new BST();
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(70);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.print("Inorder Traversal: ");
+        tree.inorder();
+    }
+}
