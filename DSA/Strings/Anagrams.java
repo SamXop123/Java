397ea878fbe199bb236557184feb885d3dc38680
Sameer Prajapati <sameerprajapati066@gmail.com>
2025-11-21 00:43:30 +0530

    Create Anagrams.java

---
+++ DSA/Strings/Anagrams.java
@@ -0,0 +1,43 @@
+package DSA.Strings;
+import java.util.*;
+import java.util.Arrays;
+
+public class Anagrams {
+
+    public static void checkAnagrams(String str1, String str2) {
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Not Anagrams");
+            return;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        if (Arrays.equals(charArray1, charArray2)) {
+            System.out.println("Anagrams");
+        } else {
+            System.out.println("Not Anagrams");
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String str1 = sc.next();
+        String str2 = sc.next();
+        
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        str1 = str1.replaceAll("\\s", "");
+        str2 = str2.replaceAll("\\s", "");
+
+        checkAnagrams(str1, str2);
+        
+        sc.close();
+    }
+}
